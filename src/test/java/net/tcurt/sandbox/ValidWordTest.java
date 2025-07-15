419da20378ab4eb4aeb4bcb2157053663add8f90
Tyler Curtis <tjcurt@gmail.com>
2025-07-15 12:18:46 -0700

    Run google-java-format
    ```
    google-java-format --replace (find . -name "*.java")
    ```

--- src/test/java/net/tcurt/sandbox/ValidWordTest.java
+++ src/test/java/net/tcurt/sandbox/ValidWordTest.java
@@ -5,21 +5,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 public class ValidWordTest {
-    private ValidWord validWord = new ValidWord();
+  private ValidWord validWord = new ValidWord();
 
-    @Test
-    void valid() {
-        assertThat(validWord.isValid("abc")).isTrue();
-        assertThat(validWord.isValid("ABC")).isTrue();
-        assertThat(validWord.isValid("AAABBBCCC1234aaabbbcccddd")).isTrue();
-    }
+  @Test
+  void valid() {
+    assertThat(validWord.isValid("abc")).isTrue();
+    assertThat(validWord.isValid("ABC")).isTrue();
+    assertThat(validWord.isValid("AAABBBCCC1234aaabbbcccddd")).isTrue();
+  }
 
-    @Test
-    void invalid() {
-        assertThat(validWord.isValid("ab")).isFalse();
-        assertThat(validWord.isValid("aaa")).isFalse();
-        assertThat(validWord.isValid("bbb")).isFalse();
-        assertThat(validWord.isValid("!abc")).isFalse();
-        assertThat(validWord.isValid("abc!")).isFalse();
-    }
+  @Test
+  void invalid() {
+    assertThat(validWord.isValid("ab")).isFalse();
+    assertThat(validWord.isValid("aaa")).isFalse();
+    assertThat(validWord.isValid("bbb")).isFalse();
+    assertThat(validWord.isValid("!abc")).isFalse();
+    assertThat(validWord.isValid("abc!")).isFalse();
+  }
 }
