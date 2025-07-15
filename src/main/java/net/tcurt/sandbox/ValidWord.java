7107362567fc00d13fdbc1b31bec47ba5b3a3af1
Tyler Curtis <tjcurt@gmail.com>
2025-07-15 12:15:40 -0700

    Leetcode 3136: Valid Word

---
+++ src/main/java/net/tcurt/sandbox/ValidWord.java
@@ -0,0 +1,30 @@
+package net.tcurt.sandbox;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.regex.Pattern;
+
+/**
+ * From <a href="https://leetcode.com/problems/valid-word/description/?envType=daily-question&envId=2025-07-15">
+ *     leetcode</a>.
+ */
+@Slf4j
+public class ValidWord {
+    private static final Pattern VOWEL_PATTERN = Pattern.compile("[aeiou]");
+    private static final Pattern CONSANANT_PATTERN = Pattern.compile("[a-z&&[^aeiou]]");
+    private static final Pattern INVALID_CHAR_PATTERN = Pattern.compile("[^a-z0-9]");
+
+    public boolean isValid(String word) {
+        if (word.length() < 3) {
+            return false;
+        }
+
+        String lowerWord = word.toLowerCase();
+        boolean vowel = VOWEL_PATTERN.matcher(lowerWord).find();
+        boolean consanant = CONSANANT_PATTERN.matcher(lowerWord).find();
+        boolean invalid = INVALID_CHAR_PATTERN.matcher(lowerWord).find();
+
+        log.info("'{}' vowel={} consanant={} invalid={}", word, vowel, consanant, invalid);
+        return vowel && consanant && !invalid;
+    }
+}
