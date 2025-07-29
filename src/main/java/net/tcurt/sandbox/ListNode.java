a18aea7700ea54203cb660159f42dd20c3860ecc
Tyler Curtis <tjcurt@gmail.com>
2025-07-29 10:18:43 -0700

    Leetcode 24: Swap nodes in pairs (backtracking)
    https://leetcode.com/problems/swap-nodes-in-pairs

---
+++ src/main/java/net/tcurt/sandbox/ListNode.java
@@ -0,0 +1,11 @@
+package net.tcurt.sandbox;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class ListNode {
+  public final int val;
+  public ListNode next;
+}
