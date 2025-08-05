003050b019370c62e0e78c7e517b5f9bc3c97916
Tyler Curtis <tjcurt@gmail.com>
2025-08-05 07:33:10 -0700

    Leetcode 904: Fruits into baskets 2
    https://leetcode.com/problems/fruit-into-baskets

---
+++ src/test/java/net/tcurt/sandbox/problems/FruitIntoBaskets2Test.java
@@ -0,0 +1,23 @@
+package net.tcurt.sandbox.problems;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class FruitIntoBaskets2Test {
+  private FruitIntoBaskets2 underTest = new FruitIntoBaskets2();
+
+  @Test
+  void fruits_4_2_5_baskets_3_5_4_is_1() {
+    int[] fruits = new int[] {4, 2, 5};
+    int[] baskets = new int[] {3, 5, 4};
+    assertThat(underTest.numOfUnplacedFruits(fruits, baskets)).isEqualTo(1);
+  }
+
+  @Test
+  void fruits_3_6_1_baskets_6_4_7_is_0() {
+    int[] fruits = new int[] {3, 6, 1};
+    int[] baskets = new int[] {6, 4, 7};
+    assertThat(underTest.numOfUnplacedFruits(fruits, baskets)).isEqualTo(0);
+  }
+}
