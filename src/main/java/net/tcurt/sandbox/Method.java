429ffdcab2f10084a7867343c207e41adcee3251
Tyler Curtis <tjcurt@gmail.com>
2025-08-10 15:57:13 -0700

    Leetcode 869: Reordered power of 2
    https://leetcode.com/problems/reordered-power-of-2

--- src/main/java/net/tcurt/sandbox/Method.java
+++ src/main/java/net/tcurt/sandbox/Method.java
@@ -4,6 +4,8 @@ public enum Method {
   BRUTE_FORCE,
   BINARY_SEARCH,
   BINARY_TREE,
+  HASH_MAP,
+  OPTIMIZED,
   RECURSIVE,
   TWO_POINTERS
 }
---
+++ src/test/java/net/tcurt/sandbox/problems/ReorderedPowerOf2Test.java
@@ -0,0 +1,39 @@
+package net.tcurt.sandbox.problems;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import net.tcurt.sandbox.Method;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class ReorderedPowerOf2Test {
+
+  static Stream<Method> methodProvider() {
+    return Stream.of(Method.HASH_MAP, Method.OPTIMIZED);
+  }
+
+  @ParameterizedTest
+  @MethodSource("methodProvider")
+  void n1_is_true(Method method) {
+    ReorderedPowerOf2 underTest = new ReorderedPowerOf2(method);
+    assertThat(underTest.reorderedPowerOf2(1)).isTrue();
+  }
+
+  @ParameterizedTest
+  @MethodSource("methodProvider")
+  void n10_is_false(Method method) {
+    ReorderedPowerOf2 underTest = new ReorderedPowerOf2(method);
+    assertThat(underTest.reorderedPowerOf2(10)).isFalse();
+  }
+
+  @ParameterizedTest
+  @MethodSource("methodProvider")
+  void n65536_is_true(Method method) {
+    ReorderedPowerOf2 underTest = new ReorderedPowerOf2(method);
+    // 65536
+    assertThat(underTest.reorderedPowerOf2(65536)).isTrue();
+    assertThat(underTest.reorderedPowerOf2(56653)).isTrue();
+    assertThat(underTest.reorderedPowerOf2(36655)).isTrue();
+  }
+}
