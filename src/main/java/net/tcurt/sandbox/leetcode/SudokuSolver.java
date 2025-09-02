2b308aaf58ca6ebc5d12910ad209d1c1c043fd4f
Tyler Curtis <tjcurt@gmail.com>
2025-09-02 10:48:39 -0700

    Include ChatGPT design for SudokuSolver

--- src/main/java/net/tcurt/sandbox/leetcode/SudokuSolver.java
+++ src/main/java/net/tcurt/sandbox/leetcode/SudokuSolver.java
@@ -10,182 +10,119 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SudokuSolver {
 
+  private static final int N = 9;
+
   private record Cell(int row, int col) {}
 
-  public static class Square {
-    private char answer = '\0';
-    private Set<Character> possible = new HashSet<>();
-
-    public Square(char answer) {
-      this.answer = answer;
-    }
-
-    public Square() {
-      this.possible.addAll(Set.of('1', '2', '3', '4', '5', '6', '7', '8', '9'));
-    }
-
-    public boolean isAnswer() {
-      return answer != '\0';
-    }
-
-    public String toString() {
-      char entry = (answer != '\0') ? answer : '.';
-      return Character.toString(entry);
-    }
-  }
-
-  private static List<List<Cell>> ALL_UNITS = new ArrayList<>();
-
-  static {
-    // cells for 9 rows
-    for (int row = 0; row < 9; row++) {
-      List<Cell> rowCells = new ArrayList<>();
-      for (int col = 0; col < 9; col++) {
-        rowCells.add(new Cell(row, col));
-      }
-      ALL_UNITS.add(rowCells);
-    }
-
-    // cells for 9 cols
-    for (int col = 0; col < 9; col++) {
-      List<Cell> colCells = new ArrayList<>();
-      for (int row = 0; row < 9; row++) {
-        colCells.add(new Cell(row, col));
-      }
-      ALL_UNITS.add(colCells);
-    }
-
-    // cells for 9 3×3 Blocks
-    for (int row = 0; row < 9; row += 3) {
-      for (int col = 0; col < 9; col += 3) {
-        List<Cell> blockCells = new ArrayList<>();
-        for (int i = row; i < row + 3; i++) {
-          for (int j = col; j < col + 3; j++) {
-            blockCells.add(new Cell(i, j));
-          }
-        }
-        ALL_UNITS.add(blockCells);
-      }
-    }
-  }
-
-  private int print(Square[][] mine) {
-    System.out.println();
-    int possiblesLeft = 0;
-    for (int i = 0; i < 9; i++) {
-      for (int j = 0; j < 9; j++) {
-        Square square = mine[i][j];
-        if (!square.isAnswer() && square.possible.size() == 1) {
-          square.answer = square.possible.iterator().next();
-        }
-        int num = square.isAnswer() ? 0 : mine[i][j].possible.size();
-        System.out.printf("%d ", num);
-        possiblesLeft += num;
-      }
-      System.out.println();
-    }
-    return possiblesLeft;
-  }
-
-  // Shared elimination for a row, column, or block
-  private void processUnit(Square[][] mine, List<Cell> cells) {
-    Set<Character> found = new HashSet<>();
-
-    // Collect solved numbers in this unit
-    for (Cell cell : cells) {
-      Square sq = mine[cell.row()][cell.col()];
-      if (sq.isAnswer()) {
-        found.add(sq.answer);
-      }
-    }
-
-    // Remove them from unsolved cells in this unit
-    for (Cell cell : cells) {
-      mine[cell.row()][cell.col()].possible.removeAll(found);
-    }
-  }
-
-  private void processAll(Square[][] mine) {
-    for (List<Cell> unit : ALL_UNITS) {
-      processUnit(mine, unit);
-    }
-  }
-
-  private boolean isValid(Square[][] mine, int row, int col, char val) {
-    // row
-    for (int j = 0; j < 9; j++) {
-      if (j != col && mine[row][j].answer == val) return false;
-    }
-
-    // col
-    for (int i = 0; i < 9; i++) {
-      if (i != row && mine[i][col].answer == val) return false;
-    }
-
-    // 3x3 square
-    int rStart = (row / 3) * 3;
-    int cStart = (col / 3) * 3;
-    for (int i = rStart; i < rStart + 3; i++) {
-      for (int j = cStart; j < cStart + 3; j++) {
-        if ((i != row || j != col) && mine[i][j].answer == val) return false;
-      }
-    }
-    return true;
-  }
-
-  private boolean backtrack(Square[][] mine) {
-    for (int i = 0; i < 9; i++) {
-      for (int j = 0; j < 9; j++) {
-        if (!mine[i][j].isAnswer()) {
-          for (char c : mine[i][j].possible) {
-            if (isValid(mine, i, j, c)) {
-              mine[i][j].answer = c;
-              if (backtrack(mine)) return true;
-              mine[i][j].answer = '\0'; // undo
-            }
-          }
-          return false; // no option worked
-        }
-      }
-    }
-    return true; // solved
-  }
-
   public void solveSudoku(char[][] board) {
-    Square[][] mine = new Square[9][9];
-    for (int i = 0; i < 9; i++) {
-      for (int j = 0; j < 9; j++) {
-        if (board[i][j] == '.') {
-          mine[i][j] = new Square();
+    // sets to track which numbers are used in each row, col, and box
+    List<Set<Character>> rowUsed = new ArrayList<>();
+    List<Set<Character>> colUsed = new ArrayList<>();
+    List<Set<Character>> boxUsed = new ArrayList<>();
+
+    for (int i = 0; i < N; i++) {
+      rowUsed.add(new HashSet<>());
+      colUsed.add(new HashSet<>());
+      boxUsed.add(new HashSet<>());
+    }
+
+    // initialize sets and collect empty cells
+    List<Cell> empties = new ArrayList<>();
+    for (int r = 0; r < N; r++) {
+      for (int c = 0; c < N; c++) {
+        char val = board[r][c];
+        if (val == '.') {
+          empties.add(new Cell(r, c));
         } else {
-          mine[i][j] = new Square(board[i][j]);
+          rowUsed.get(r).add(val);
+          colUsed.get(c).add(val);
+          boxUsed.get(boxIndex(r, c)).add(val);
         }
       }
     }
 
-    int prevPossiblesLeft = print(mine);
-    boolean done = false;
-    while (!done) {
-      processAll(mine);
+    backtrack(board, empties, rowUsed, colUsed, boxUsed);
+  }
 
-      int possiblesLeft = print(mine);
-      if (possiblesLeft == prevPossiblesLeft) {
-        // didn't make any progress... need to guess + add backtracking
-        backtrack(mine);
-      } else {
-        prevPossiblesLeft = possiblesLeft;
+  // 0 | 1 | 2
+  // 3 | 4 | 5
+  // 6 | 7 | 8
+  private int boxIndex(int r, int c) {
+    return (r / 3) * 3 + (c / 3);
+  }
+
+  private boolean backtrack(
+      char[][] board,
+      List<Cell> empties,
+      List<Set<Character>> rowUsed,
+      List<Set<Character>> colUsed,
+      List<Set<Character>> boxUsed) {
+    if (empties.size() == 0) return true; // solved!
+
+    // MRV (minimum remaining values): pick cell with fewest options
+    int bestNdx = -1;
+    int bestCount = N + 1;
+    Set<Character> bestCandidates = null;
+
+    for (int ndx = 0; ndx < empties.size(); ndx++) {
+      int row = empties.get(ndx).row();
+      int col = empties.get(ndx).col();
+      Set<Character> candidates = candidatesFor(row, col, rowUsed, colUsed, boxUsed);
+
+      if (candidates.size() < bestCount) {
+        bestCount = candidates.size();
+        bestNdx = ndx;
+        bestCandidates = candidates;
+        if (bestCount == 1) break; // can't get better
       }
-      done = possiblesLeft == 0;
     }
 
-    if (done) {
-      for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-          board[i][j] = mine[i][j].answer;
-        }
-      }
-    } else {
-      throw new IllegalStateException("never finished");
+    Cell bestCell = empties.remove(bestNdx);
+    int r = bestCell.row(), c = bestCell.col();
+    int b = boxIndex(r, c);
+    System.out.printf(
+        "  Placing %d candidates in [row=%d col=%d box=%d]: %s\n",
+        bestCount, r, c, b, bestCandidates);
+
+    // NOTE: if we have no candidates here, we'll return false and re-add it back to empties
+    for (char ch : bestCandidates) {
+      // try placing candidate
+      board[r][c] = ch;
+      rowUsed.get(r).add(ch);
+      colUsed.get(c).add(ch);
+      boxUsed.get(b).add(ch);
+
+      if (backtrack(board, empties, rowUsed, colUsed, boxUsed)) return true;
+      System.out.printf("  Undo failed candidate=%s in [row=%d col=%d box=%d]\n", ch, r, c, b);
+
+      // backtrack / undo guess that was incorrect
+      board[r][c] = '.';
+      rowUsed.get(r).remove(ch);
+      colUsed.get(c).remove(ch);
+      boxUsed.get(b).remove(ch);
     }
+
+    empties.add(bestNdx, bestCell); // restore order
+    return false;
+  }
+
+  private Set<Character> candidatesFor(
+      int row,
+      int col,
+      List<Set<Character>> rowUsed,
+      List<Set<Character>> colUsed,
+      List<Set<Character>> boxUsed) {
+
+    Set<Character> candidates = new HashSet<>();
+    for (char num = '1'; num <= '9'; num++) {
+      int boxNdx = boxIndex(row, col);
+      if (!rowUsed.get(row).contains(num)
+          && !colUsed.get(col).contains(num)
+          && !boxUsed.get(boxNdx).contains(num)) {
+        candidates.add(num);
+      }
+    }
+
+    return candidates;
   }
 }
